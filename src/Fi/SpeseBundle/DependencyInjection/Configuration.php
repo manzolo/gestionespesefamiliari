260dc48172bed6b7e24b9ed2a39b7981600cf396
d59495 <andrea.manzi@comune.fi.it>
2016-09-07 12:42:22 +0200

    Modifiche per travis e codesniffer

--- src/Fi/SpeseBundle/DependencyInjection/Configuration.php
+++ src/Fi/SpeseBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('fi_spese');
+        $treeBuilder->root('fi_spese');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
