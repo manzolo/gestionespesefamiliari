c33533a8a15106282840966467df26fb78740576
Andrea Manzi <manzolo@libero.it>
2016-09-08 23:13:31 +0200

    add tests

---
+++ src/Fi/SpeseBundle/Tests/Entity/CategoriaTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Fi\SpeseBundle\Tests\Entity;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class CategoriaTest extends KernelTestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    /**
+     * @test
+     */
+    public function CategoriaInsertDeleteTest()
+    {
+        $em = $this->em;
+        $categoria = new \Fi\SpeseBundle\Entity\Categoria();
+        $categoria->setDescrizione('Prova categoria');
+        $em->persist($categoria);
+        $em->flush();
+        $this->assertGreaterThanOrEqual(1, $categoria->getId());
+        $em->remove($categoria);
+        $em->flush();
+        $this->assertTrue(is_null($categoria->getId()));
+    }
+}
