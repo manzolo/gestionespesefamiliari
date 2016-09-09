071138bc9aab9a00a6d552e121c9347b82f7b500
d59495 <andrea.manzi@comune.fi.it>
2016-09-09 14:14:14 +0200

    add Mink

--- src/Fi/SpeseBundle/Tests/Controller/CategoriaControllerTest.php
+++ src/Fi/SpeseBundle/Tests/Controller/CategoriaControllerTest.php
@@ -15,7 +15,25 @@ class CategoriaControllerTest extends SpeseTest
         parent::__construct();
         $this->setClassName(get_class());
         $client = $this->getClientAutorizzato();
-        $client->request('GET', '/Categoria/');
+        $url = $client->getContainer()->get('router')->generate('Categoria_container');
+        $client->request('GET', $url);
+        $crawler = new Crawler($client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $body = $crawler->filter('div[id="Categoria"]');
+        $attributes = $body->extract(array('_text', 'class'));
+        $this->assertEquals($attributes[0][1], 'tabella');
+    }
+    
+    /**
+     * @test
+     */
+    public function testAddCategoria()
+    {
+        parent::__construct();
+        $this->setClassName(get_class());
+        $client = $this->getClientAutorizzato();
+        $url = $client->getContainer()->get('router')->generate('Categoria_container');
+        $client->request('GET', $url);
         $crawler = new Crawler($client->getResponse()->getContent());
         $this->assertTrue($client->getResponse()->isSuccessful());
         $body = $crawler->filter('div[id="Categoria"]');
