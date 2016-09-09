331b3f672a499ee9edcd6ff5cfe59c23332368d0
d59495 <andrea.manzi@comune.fi.it>
2016-09-09 14:27:29 +0200

    add Mink

--- src/Fi/SpeseBundle/Tests/Controller/CategoriaControllerTest.php
+++ src/Fi/SpeseBundle/Tests/Controller/CategoriaControllerTest.php
@@ -7,10 +7,11 @@ use Fi\SpeseBundle\DependencyInjection\SpeseTest;
 
 class CategoriaControllerTest extends SpeseTest
 {
+
     /**
      * @test
      */
-    public function testIndexCategoria()
+    public function testIndexCategoria() 
     {
         parent::__construct();
         $this->setClassName(get_class());
@@ -23,21 +24,24 @@ class CategoriaControllerTest extends SpeseTest
         $attributes = $body->extract(array('_text', 'class'));
         $this->assertEquals($attributes[0][1], 'tabella');
     }
-    
+
     /**
      * @test
      */
-    public function testAddCategoria()
+    public function testAddCategoria() 
     {
-        parent::__construct();
-        $this->setClassName(get_class());
-        $client = $this->getClientAutorizzato();
-        $url = $client->getContainer()->get('router')->generate('Categoria_container');
-        $client->request('GET', $url);
-        $crawler = new Crawler($client->getResponse()->getContent());
-        $this->assertTrue($client->getResponse()->isSuccessful());
-        $body = $crawler->filter('div[id="Categoria"]');
-        $attributes = $body->extract(array('_text', 'class'));
-        $this->assertEquals($attributes[0][1], 'tabella');
+        /* parent::__construct();
+          $this->setClassName(get_class());
+          $client = $this->getClientAutorizzato();
+          $driver = new \Behat\Mink\Driver\BrowserKitDriver($client);
+
+          $session = new \Behat\Mink\Session($driver);
+
+          $session->start();
+          $url = $client->getContainer()->get('router')->generate('Categoria_container');
+          $session->visit($url);
+          echo $session->evaluateScript("jQuery('#addjqgridrow')");
+         */
     }
+
 }
