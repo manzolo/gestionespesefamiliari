4c5bf1603511d2e03c1b0b934323b706d1eff204
Andrea Manzi <manzolo@libero.it>
2016-09-09 23:11:37 +0200

    new test

--- src/Fi/SpeseBundle/Tests/Entity/FamigliaTest.php
+++ src/Fi/SpeseBundle/Tests/Entity/FamigliaTest.php
@@ -29,12 +29,24 @@ class FamigliaTest extends KernelTestCase
     public function famigliaInsertDeleteTest()
     {
         $em = $this->em;
+        $descrizione = 'ProvaFamiglia';
         $famiglia = new \Fi\SpeseBundle\Entity\Famiglia();
-        $famiglia->setDescrizione('Prova Famiglia');
+        $famiglia->setDescrizione($descrizione);
         $famiglia->setDal(\DateTime::createFromFormat('Y-m-d', date('Y-m-d')));
+        $em->persist($famiglia);
+        $famiglia->setAl(\DateTime::createFromFormat('Y-m-d', date('Y-m-d')));
         $em->persist($famiglia);
         $em->flush();
         $this->assertGreaterThanOrEqual(1, $famiglia->getId());
+
+        $qu = $em->createQueryBuilder();
+        $qu->select(array('f'))
+            ->from('FiSpeseBundle:Famiglia', 'f')
+            ->where('f.descrizione = :descrizione')
+            ->setParameter('descrizione', $descrizione);
+        $famigliaq = $qu->getQuery()->getSingleResult();
+        $this->assertEquals($famigliaq->getDescrizione(), $descrizione);
+
         $em->remove($famiglia);
         $em->flush();
         $this->assertTrue(is_null($famiglia->getId()));
