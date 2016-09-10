857d7f4f6129e412b1956e53bf9a423577f4c87f
Andrea Manzi <manzolo@libero.it>
2016-09-10 07:29:20 +0200

    new test

--- src/Fi/SpeseBundle/Tests/Controller/CategoriaControllerTest.php
+++ src/Fi/SpeseBundle/Tests/Controller/CategoriaControllerTest.php
@@ -26,75 +26,71 @@ class CategoriaControllerTest extends SpeseTest
         $this->assertEquals($attributes[0][1], 'tabella');
     }
 
-    /**
+    /*
      * @test
      */
-    public function testAddCategoria()
-    {
-        parent::__construct();
-        $this->setClassName(get_class());
-        $browser = 'firefox';
-        //$url = $client->getContainer()->get('router')->generate('Categoria_container');
-        $url = 'http://127.0.0.1:8000/app_test.php/Categoria';
+    /*    public function testAddCategoria()
+      {
+      parent::__construct();
+      $this->setClassName(get_class());
+      $browser = 'firefox';
+      //$url = $client->getContainer()->get('router')->generate('Categoria_container');
+      $url = 'http://127.0.0.1:8000/app_test.php/Categoria';
 
-        // Choose a Mink driver. More about it in later chapters.
-        $driver = new \Behat\Mink\Driver\Selenium2Driver($browser);
-        $session = new \Behat\Mink\Session($driver);
-        // start the session
-        $session->start();
-        $session->visit($url);
-        $page = $session->getPage();
-        $page->fillField('username', 'admin');
-        $page->fillField('password', 'admin');
-        $page->pressButton('_submit');
-        //$page = $session->getPage();
+      // Choose a Mink driver. More about it in later chapters.
+      $driver = new \Behat\Mink\Driver\Selenium2Driver($browser);
+      $session = new \Behat\Mink\Session($driver);
+      // start the session
+      $session->start();
+      $session->visit($url);
+      $page = $session->getPage();
+      $page->fillField('username', 'admin');
+      $page->fillField('password', 'admin');
+      $page->pressButton('_submit');
+      //$page = $session->getPage();
 
-        $element = $page->findAll('css', '.ui-icon-plus');
+      $element = $page->findAll('css', '.ui-icon-plus');
 
-        foreach ($element as $e) {
-            if ($e->isVisible()) {
-                $e->click();
-            }
-        }
+      foreach ($element as $e) {
+      if ($e->isVisible()) {
+      $e->click();
+      }
+      }
 
-        sleep(1);
-        $descrizionetest = 'CategoriaProva';
-        $page->fillField('fi_spesebundle_categoria_descrizione', $descrizionetest);
-        $page->find('css', 'a#sDataCategoriaS')->click();
-        sleep(1);
-        $session->stop();
+      sleep(1);
+      $descrizionetest = 'CategoriaProva';
+      $page->fillField('fi_spesebundle_categoria_descrizione', $descrizionetest);
+      $page->find('css', 'a#sDataCategoriaS')->click();
+      sleep(1);
+      $session->stop();
 
-        $client = $this->getClientAutorizzato();
-        /* @var $em \Doctrine\ORM\EntityManager */
-        $em = $client->getContainer()->get('doctrine')->getManager();
+      $client = $this->getClientAutorizzato();
+      // @var $em \Doctrine\ORM\EntityManager
+      $em = $client->getContainer()->get('doctrine')->getManager();
 
-        $qu = $em->createQueryBuilder();
-        $qu->select(array('c'))
-            ->from('FiSpeseBundle:Categoria', 'c')
-            ->where('c.descrizione = :descrizione')
-            ->setParameter('descrizione', $descrizionetest);
-        $categoria = $qu->getQuery()->getSingleResult();
-        $this->assertEquals($categoria->getDescrizione(), $descrizionetest);
+      $qu = $em->createQueryBuilder();
+      $qu->select(array('c'))
+      ->from('FiSpeseBundle:Categoria', 'c')
+      ->where('c.descrizione = :descrizione')
+      ->setParameter('descrizione', $descrizionetest);
+      $categoria = $qu->getQuery()->getSingleResult();
+      $this->assertEquals($categoria->getDescrizione(), $descrizionetest);
 
-        $em->remove($categoria);
-        $em->flush();
-        $em->clear();
-        $this->assertTrue(is_null($categoria->getId()));
+      $em->remove($categoria);
+      $em->flush();
+      $em->clear();
+      $this->assertTrue(is_null($categoria->getId()));
 
-        //$session->wait(5000, "$('.ui-icon-plus').visible");
-        //$findName = $page->find("css", ".ui-icon-plus");
-        //$findName->click();
-        //$session->evaluateScript("$('#grid1').jqGrid('getGridParam', 'selrow')");
-        //$findName = $page->find("css", "#addjqgridrow");
-        //$findName->click();
-        //$session->evaluateScript("jQuery('#addjqgridrow');");
-        //$js = 'jQuery("#list1").jqGrid("addRow","new");';
-        //$session->evaluateScript($js);
-        //$page->pressButton("jQuery('#list1').jqGrid('addjqgridrow');");
-        //$session->stop();
-        /* $session->start();
-          $url = $client->getContainer()->get('router')->generate('Categoria_container');
-          $session->visit($url);
-          echo $session->evaluateScript("jQuery('#addjqgridrow')"); */
-    }
+      //$session->wait(5000, "$('.ui-icon-plus').visible");
+      //$findName = $page->find("css", ".ui-icon-plus");
+      //$findName->click();
+      //$session->evaluateScript("$('#grid1').jqGrid('getGridParam', 'selrow')");
+      //$findName = $page->find("css", "#addjqgridrow");
+      //$findName->click();
+      //$session->evaluateScript("jQuery('#addjqgridrow');");
+      //$js = 'jQuery("#list1").jqGrid("addRow","new");';
+      //$session->evaluateScript($js);
+      //$page->pressButton("jQuery('#list1').jqGrid('addjqgridrow');");
+      //$session->stop();
+      } */
 }
