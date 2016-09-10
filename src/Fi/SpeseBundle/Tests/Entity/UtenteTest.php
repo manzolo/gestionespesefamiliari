b976912a425d61286f0af937c76090720e1dcbe6
Andrea Manzi <manzolo@libero.it>
2016-09-10 13:23:22 +0200

    add tests

--- src/Fi/SpeseBundle/Tests/Entity/UtenteTest.php
+++ src/Fi/SpeseBundle/Tests/Entity/UtenteTest.php
@@ -30,20 +30,39 @@ class UtenteTest extends KernelTestCase
     {
         $em = $this->em;
         $famiglia = new \Fi\SpeseBundle\Entity\Famiglia();
-        $famiglia->setDescrizione('Prova famiglia');
-        $famiglia->setDal(\DateTime::createFromFormat('Y-m-d', date('Y-m-d')));
+        $nomefamiglia = 'Prova famiglia';
+        $data = \DateTime::createFromFormat('Y-m-d', date('Y-m-d'));
+        $famiglia->setDescrizione($nomefamiglia);
+        $famiglia->setDal($data);
+        $famiglia->setAl($data);
 
         $utente = new \Fi\SpeseBundle\Entity\Utente();
         $utente->setFamiglia($famiglia);
-        $utente->setCognome('Prova Cognome');
-        $utente->setNome('Prova Nome');
-        $utente->setEmail('email@email.it');
-        $utente->setUsername('Username');
-        $utente->setPassword('Password');
+
+        $cognome = 'Prova Cognome';
+        $nome = 'Prova Nome';
+        $email = 'email@email.it';
+        $username = 'Username';
+        $password = 'Password';
+
+        $utente->setCognome($cognome);
+        $utente->setNome($nome);
+        $utente->setEmail($email);
+        $utente->setUsername($username);
+        $utente->setPassword($password);
         $em->persist($famiglia);
         $em->persist($utente);
         $em->flush();
+
         $this->assertGreaterThanOrEqual(1, $utente->getId());
+        $this->assertEquals($cognome, $utente->getCognome());
+        $this->assertEquals($nome, $utente->getNome());
+        $this->assertEquals($email, $utente->getEmail());
+        $this->assertEquals($username, $utente->getUsername());
+        $this->assertEquals($password, $utente->getPassword());
+
+        $this->assertGreaterThanOrEqual(1, $utente->getId());
+
         $em->remove($utente);
         $em->remove($famiglia);
         $em->flush();
