05ea6fdba8814c0d23f7471875444b91a427da73
d59495 <andrea.manzi@comune.fi.it>
2017-08-31 18:18:38 +0200

    rimossa gestione allegati

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,8 +19,6 @@ class AppKernel extends Kernel {
             new FOS\UserBundle\FOSUserBundle(),
             new Fi\FOSUserBundle\FiFOSUserBundle(),
             new Fi\SpeseBundle\FiSpeseBundle(),
-            new Vich\UploaderBundle\VichUploaderBundle(),
-            new Oneup\FlysystemBundle\OneupFlysystemBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test', 'localhost'))) {
